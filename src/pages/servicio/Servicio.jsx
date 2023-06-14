5655ebf7a375d0a85212df61d41bd0b83473040d
72422998 <81695790+72422998@users.noreply.github.com>
2023-06-13 22:54:33 -0500

    feat: Implementacion del responsive de la seccion de servicios
    Se añadio los mockups de la seccion de servicios para pantallas de tablet y desktop

--- src/pages/servicio/Servicio.jsx
+++ src/pages/servicio/Servicio.jsx
@@ -16,16 +16,25 @@ const Servicio = () => {
   },[])
   
   return (
-    <section className='Nutricionista_tarjetas'>
-      
+    <section className='Nutricionista_servicio'>
+      <select name="Tipo de servicio" id="tipo_servicio" className='Nutricionista_select'>
+        <option value="Tipo_de_servicio">Tipo de servicio</option>
+        <option value="Nutricion_deportivo">Nutricion Deportiva</option>
+        <option value="Nutricion_infantil">Nutricion Infantil</option>
+        <option value="Nutricion_vegetariana">Dieta vegetariana</option>
+        <option value="Nutricion_bajar_peso">Bajar de peso</option>
+        <option value="Nutricion_subir_peso">Subir de peso</option>
+        <option value="Nutricion_enfermedades_cronicas">Enfermedades cronicas</option>
+      </select>
+    <div className='Nutricionista_tarjetas'>
       {servicios.map((servicio) => (
-        
           <div key={servicio.id} className='Nutricionista_tarjeta'>
             <img className='Nutricionista_imagen' src={cliente} alt="Nutricionista"/>
             <h3 className='Nutricionista_nombre' >{servicio.profesion}</h3>
           </div>
         
       ))}
+    </div>
     </section>
   );
 }
