44b722c3e63aadd1b0de7cded84afa200b382bc8
bmancilla <bmancilla119@gmail.com>
2023-06-18 17:13:29 -0500

    feat: Control de errores
    Integración de preloader y respuesta en caso de problemas de conexion con apis

--- src/pages/servicio/Servicio.jsx
+++ src/pages/servicio/Servicio.jsx
@@ -1,19 +1,26 @@
 import React,{useState,useEffect} from 'react'
 import { FetchService } from '../../api/FetchService'
-import cliente from "../../assets/cliente1.jpg"
 import './servicio.css'
 import { Link } from 'react-router-dom'
+import {DotLoader} from 'react-spinners'
+import robot from '../../assets/robotica.png'
 const Servicio = () => {
   const [servicios,setServicios]= useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
   useEffect(()=>{
     const obtenerServicios = async()=>{
-      const data = await FetchService();
-      
-      setServicios(data);
-    
+      try {
+        const data = await FetchService();
+        setServicios(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error al obtener los servicios:', error);
+        setIsLoading(false);
+      }
     }
     obtenerServicios()
-  },[])
+  },[]);
   
   return (
     <section className='Nutricionista_servicio'>
@@ -28,7 +35,18 @@ const Servicio = () => {
       </select>
       
     <div className='Nutricionista_tarjetas'>
-      {servicios.map((servicio) => (
+      {
+        isLoading ? (
+          <div className='loader-container'>
+            <DotLoader color='#1B7E69' loading={isLoading}></DotLoader>
+          </div>
+        ):servicios.length === 0 ? (
+          <div className="NoServicios_container">
+            <p className='NoServicios_texto'>No se encontraron servicios</p>
+            <img className='NoServicios_imagen' src={robot} alt="" />
+          </div>
+        ):(
+          servicios.map((servicio) => (
           <div key={servicio.id} className='Nutricionista_tarjeta'>
             <Link to={`/servicios/detalle/${servicio.id}`}>
               <img src={servicio.nutricionista.foto} alt="nutricionista" className='Nutricionista_imagen'/>
@@ -37,18 +55,9 @@ const Servicio = () => {
                 <p>{servicio.nutricionista.categoria}</p>
               </div>
             </Link>
-            {/* <div className='Nutricionista_imagen'>
-            <img  src={servicio.nutricionista.foto} alt="Nutricionista"></img>
-            </div>
-            <div className='Nutricionista_interact'>
-              <h3 className='Nutricionista_nombre' >{servicio.nutricionista.profesion}</h3>
-              <p className='Nutricionista_servicos'>{servicio.nombre}</p>
-              <button><Link to={`/servicios/detalle/${servicio.id}`} className='Nutricionista_link'>Ver detalle</Link></button>
-            </div> */}
           </div>
-        
-      ))}
-      
+          ))
+      )}
     </div>
     </section>
   );
