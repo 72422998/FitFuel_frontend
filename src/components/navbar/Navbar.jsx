eeae89530fe72ff74b1d4454d98aaa1d9a587ab5
bmancilla <bmancilla119@gmail.com>
2023-06-17 19:47:05 -0500

    feat: Funcionalidad active en css
    Se añadio detección de rutas para el nav

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,24 +1,28 @@
 import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation, NavLink } from 'react-router-dom'
 import "./navbar.css"
 
 
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false)
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  }
   return (
     <nav className='navbar'>
         <div className=''>
         <span className='logo'>FitFuel</span>
         </div>
         <ul className={`nav_items ${isOpen && 'open'}`}>
-            <li className='element'><Link to='/' className='link'>Inicio</Link></li>
-            <li className='element'><Link to='/servicios' className='link'>Servicios</Link></li>
-            <li className='element'><Link to='/sobre-nosotros' className='link'>Nosotros</Link></li>
-            <li className='element'><Link to='/contactanos' className='link'>Contáctanos</Link></li>
+            <li className='element' onClick={closeMenu}><NavLink to='/' className="link" activeClassName='active'>Inicio</NavLink></li>
+            <li className='element' onClick={closeMenu}><NavLink to='/servicios' className="link" activeClassName='active' >Servicios</NavLink></li>
+            <li className='element' onClick={closeMenu}><NavLink to='/sobre-nosotros' className="link" activeClassName='active'>Nosotros</NavLink></li>
+            <li className='element' onClick={closeMenu}><NavLink to='/contactanos' className="link" activeClassName='active'>Contáctanos</NavLink></li>
             <ul className='auth'>
-            <li className='element'><Link to='/login' className='link'>Login</Link></li>
-            <li className='element'><Link to='/register' className='link'>Register</Link></li>
+            <li className='element' onClick={closeMenu}><NavLink to='/login' className="link" activeClassName='active'>Login</NavLink></li>
+            <li className='element' onClick={closeMenu}><NavLink to='/register' className="link" activeClassName='active'>Register</NavLink></li>
             </ul>
         </ul>
         <div className={`nav_toggle ${isOpen && 'open'}`} onClick={()=>{setIsOpen(!isOpen)}}>
