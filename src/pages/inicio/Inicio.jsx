1d032e8446c69c8105d2e67db744c9d8d31ccdce
bmancilla <bmancilla119@gmail.com>
2023-06-19 03:51:47 -0500

    feat: Componentes oferta
    Integración de nuevo componente a la pagina inicio

--- src/pages/inicio/Inicio.jsx
+++ src/pages/inicio/Inicio.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import { Slider } from './slider/Slider'
 import { Testimonios } from './testimonios/Testimonios'
 import { Descripcion } from './descripcion/Descripcion'
+import { Offer } from './offer/Offer'
 
 const Inicio = () => {
   return (
-    <div>
+    <main>
       <Slider></Slider>
+      <Offer></Offer>
       <Descripcion></Descripcion>
       <Testimonios></Testimonios>
-    </div>
+    </main>
   )
 }
 
