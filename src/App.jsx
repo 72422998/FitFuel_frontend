6bad1bca1530eb99522a117fd3f1667b77650af5
bmancilla <bmancilla119@gmail.com>
2023-07-01 06:01:20 -0500

    perf: Refactorizando código
    Estilos globales añadidos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react'
-import './App.css'
+import './global/global.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import {Inicio} from './pages/inicio/Inicio'
 import { Servicio } from './pages/servicio/Servicio'
